35a8e11931f3d64cf0aab5c14dd2d73492da8ed2
Arjun <1403arjun@gmail.com>
2025-10-03 18:06:10 +0530

    Update project structure: add README and requirements files, configure database connection with environment variables

--- Library_Management_System/src/database_connection.py
+++ Library_Management_System/src/database_connection.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-db_url = 'postgresql://postgres:admin@localhost:5432/library'
+db_url = os.getenv('DB_URL', 'sqlite:///./test.db')
 
 engine = create_engine(url=db_url)
 
 Base = declarative_base()
-
 SessionLocal = sessionmaker(bind=engine)
 
 def get_db():
